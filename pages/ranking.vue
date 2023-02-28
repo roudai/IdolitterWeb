392fc1d3279246084e48abbd5f2098d850c7d361
roudai <me@roudai.net>
2023-02-28 22:20:16 +0900

    フォロワー増加数ランキング

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -6,7 +6,7 @@ const runtimeConfig = useRuntimeConfig()
 const baseUrl = runtimeConfig.public.baseURL + '取得差分一覧?'
 const params = {
   key: runtimeConfig.public.apiKey,
-  range: '取得差分!I1:N101',
+  range: '取得差分!I1:N301',
 }
 const qyeryParams = new URLSearchParams(params)
 const { data } = await useFetch(baseUrl + qyeryParams)
@@ -17,49 +17,73 @@ idolData.values.shift()
 
 // 取得データを整形
 type User = {
-  group: string
-  TwitterID: string
+  rank: number
   TwitterName: string
-  oldFollower: number
-  newFollower: number
-  followerRanking: number
+  TwitterID: string
+  group: string
+  oldNum: number
+  newNum: number
+  increaseNum: number
 }
-const output: { [key: string]: User } = idolData.values.reduce(
-  (acc: { [key: string]: User }, [group, TwitterID, TwitterName, oldFollower, newFollower, followerRanking], index) => {
-    acc[index] = {
-      group,
-      TwitterID,
-      TwitterName,
-      oldFollower: Number(oldFollower),
-      newFollower: Number(newFollower),
-      followerRanking: Number(followerRanking),
-    }
-    return acc
-  },
-  {}
-)
+const rankData: User[] = []
+let rank = 1
+let rankup = 0
+for (let i = 0; i < idolData.values.length; i++) {
+  if (rank > 100) {
+    break
+  }
+  const oldNum = Number(idolData.values[i][3])
+  const newNum = Number(idolData.values[i][4])
+  const increaseNum = Number(idolData.values[i][5])
+  if (oldNum === 0 || newNum / oldNum >= 3) {
+    continue
+  }
 
-const userList = ref<User[]>(Object.values(output))
+  rankData.push({
+    rank,
+    TwitterName: idolData.values[i][2],
+    TwitterID: idolData.values[i][1],
+    group: idolData.values[i][0],
+    oldNum,
+    newNum,
+    increaseNum,
+  })
+
+  if (idolData.values[i][5] === idolData.values[i + 1][5]) {
+    rankup = rankup + 1
+  } else {
+    rank = rank + rankup + 1
+    rankup = 0
+  }
+}
+
+const userList = ref<User[]>(Object.values(rankData))
 
 const columnHelper = createColumnHelper<User>()
 const columns: ColumnDef<User, any>[] = [
-  columnHelper.accessor('group', { header: () => 'グループ', cell: (info) => info.getValue(), enableSorting: false }),
+  columnHelper.accessor('rank', { header: () => '順位', cell: (info) => info.getValue(), enableSorting: false }),
+  columnHelper.accessor('TwitterName', {
+    header: () => 'TwitterName',
+    cell: (info) => info.getValue(),
+    enableSorting: false,
+  }),
   columnHelper.accessor('TwitterID', {
     cell: (info) => h('a', { href: 'https://twitter.com/' + info.getValue(), target: '_blank' }, info.getValue()),
     enableSorting: false,
   }),
-  columnHelper.accessor('oldFollower', {
-    header: () => 'フォロワー',
+  columnHelper.accessor('group', { header: () => 'グループ', cell: (info) => info.getValue(), enableSorting: false }),
+  columnHelper.accessor('oldNum', {
+    header: () => '前フォロワー数',
     cell: (info) => info.getValue(),
     enableSorting: false,
   }),
-  columnHelper.accessor('newFollower', {
-    header: () => 'ツイート',
+  columnHelper.accessor('newNum', {
+    header: () => '後フォロワー数',
     cell: (info) => info.getValue(),
     enableSorting: false,
   }),
-  columnHelper.accessor('followerRanking', {
-    header: () => 'ツイート',
+  columnHelper.accessor('increaseNum', {
+    header: () => 'フォロワー増加数',
     cell: (info) => info.getValue(),
     enableSorting: false,
   }),
