cdd5137b6649130265a20f80524a675ac148ff5a
roudai <me@roudai.net>
2023-03-02 00:06:03 +0900

    firebase設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,9 +71,9 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt', '@vueuse/nuxt', 'unplugin-icons/nuxt', 'nuxt-lodash'],
 
   // vueuse
-  vueuse: {
-    ssrHandlers: true,
-  },
+  // vueuse: {
+  //   ssrHandlers: true,
+  // },
 
   // vite
   vite: {
@@ -89,8 +89,15 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      baseURL: 'https://sheets.googleapis.com/v4/spreadsheets/1U6dXI2B5EOI8f8h2t20BIVbIznLbNtDegNwHHODda_8/values/',
-      apiKey: 'AIzaSyBiIUR359_DPStoJKJUtyv6Vzyfw4BU2oY',
+      baseURL: process.env.baseURL || '',
+      apiKey: process.env.apiKey || '',
+      FB_API_KEY: process.env.FB_API_KEY || '',
+      FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN || '',
+      FB_PROJECT_ID: process.env.FB_PROJECT_ID || '',
+      FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET || '',
+      FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID || '',
+      FB_APP_ID: process.env.FB_APP_ID || '',
+      FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID || '',
     },
   },
 })
